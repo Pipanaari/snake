3e3575135298e1127b3cf644ed1af94a9d22459b
Pipanaari <109987161+Pipanaari@users.noreply.github.com>
2026-04-28 20:57:26 +0300

    add snake box

--- main.c
+++ main.c
@@ -3,8 +3,23 @@
 
 int main() {
   int lastChar = 0;
+  int width = 20, height = 10;
+  int plrx = 10, plry = 5;
+  for(int y = 1; y <= height; y++){
+    for(int x = 1; x <= width; x++){
+      if( x == 1 || x == width || y == 1 || y == height){
+        printf("0");
+      }
+      else if(x == plrx && y == plry){
+        printf("#");
+      }
+      else{
+        printf(".");
+      }
+    }
+    printf("\n");
+  }
   while(lastChar != 3) {
     lastChar = _getch();
-    printf("%d\n", lastChar - 96);
   }
 }
