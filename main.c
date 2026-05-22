95b41bc8f058be0a2f4b8ac452872dea06de252d
Pipanaari <109987161+Pipanaari@users.noreply.github.com>
2026-05-22 17:43:07 +0300

    hairy snake

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main() {
 start:
   int lastChar = 0;
   int width = 20, height = 10, grid = (width - 2) * (height - 2);
-  int plrx = 10, plry = 5, score = 5, wait = 1;
+  int plrx = width / 2, plry = height / 2, score = 5, wait = 1;
   char *head = "";
   int posx[grid], posy[grid];
   memset(posx, 0, sizeof(posx));
@@ -54,22 +54,25 @@ start:
     }
     posy[x] = plry;
     posx[x] = plrx;
-    if(plrx == 1 || plrx == width || plry == 1 || plry == height){
+    if(plrx == 1 || plrx == width || plry == 1 || plry == height || score == grid){
       printf("\e[%dA", height);
       goto start;
     }
     else{
-      int calc = ((x - score) + grid) % grid;
+      int scalc = ((x - score) + grid) % grid;
+      int bcalc = ((x - 1) + grid) % grid;
       if(wait >= score){ 
-        printf("\e[%dA\e[%dC.", height + 1 - posy[calc], posx[calc] - 1);
-        printf("\e[%dD\e[%dB", posx[calc], height + 1 - posy[calc]); 
+        printf("\e[%dA\e[%dC.", height + 1 - posy[scalc], posx[scalc] - 1);
+        printf("\e[%dD\e[%dB", posx[scalc], height + 1 - posy[scalc]); 
       }
       printf("\e[%dA\e[%dC%s", height + 1 - plry, plrx - 1, head);
       printf("\e[%dD\e[%dB", plrx, height + 1 - plry); 
-      printf("%d %d %d     \n\e[A", calc, score, x);
+      printf("\e[%dA\e[%dC#", height + 1 - posy[bcalc], posx[bcalc] - 1);
+      printf("\e[%dD\e[%dB", posx[bcalc], height + 1 - posy[bcalc]); 
+      printf("%d %d %d     \n\e[A", scalc, wait, x);
       wait++;
       x++;
-      x = x % (grid + 1);
+      x = x % grid;
     }
     // printf("\e[%dA", height); 
   }
